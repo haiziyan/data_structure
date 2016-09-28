41cf46f38f2f2ab7c1f166e1374a568226a43d41
haiziyan <haiziyan@yahoo.com>
2016-09-28 14:00:18 +0800

    这是一个由路径法写成的二叉树

--- BTree/main.c
+++ BTree/main.c
@@ -17,8 +17,50 @@ void printf_data(BTreeNode *node)
 
 int main()
 {
-    BTree *btree = BTree_create();
-    BTree_destory(btree);
-    printf("Hello world!\n");
-    return 0;
+    BTree* tree = BTree_create();
+    
+    struct Node n1 = {{NULL, NULL}, 'A'};
+    struct Node n2 = {{NULL, NULL}, 'B'};
+    struct Node n3 = {{NULL, NULL}, 'C'};
+    struct Node n4 = {{NULL, NULL}, 'D'};
+    struct Node n5 = {{NULL, NULL}, 'E'};
+    struct Node n6 = {{NULL, NULL}, 'F'};
+    
+    BTree_insert(tree, (BTreeNode*)&n1, 0, 0, 0);
+    BTree_insert(tree, (BTreeNode*)&n2, 0x00, 1, 0);
+    BTree_insert(tree, (BTreeNode*)&n3, 0x01, 1, 0);
+    BTree_iInsert(tree, (BTreeNode*)&n4, 0x00, 2, 0);
+    BTree_iInsert(tree, (BTreeNode*)&n5, 0x02, 2, 0);
+    BTree_insert(tree, (BTreeNode*)&n6, 0x02, 3, 0);
+    
+    printf("Height: %d\n", BTree_height(tree));
+    printf("Degree: %d\n", BTree_degree(tree));
+    printf("Count: %d\n", BTree_count(tree));
+    printf("Position At (0x02, 2): %c\n", ((struct Node*)BTree_get(tree, 0x02, 2))->v);
+    printf("Full Tree: \n");
+    
+    BTree_display(tree, printf_data, 4, '-');
+    
+    BTree_delete(tree, 0x00, 1);
+    
+    printf("After Delete B: \n");
+    printf("Height: %d\n", BTree_height(tree));
+    printf("Degree: %d\n", BTree_degree(tree));
+    printf("Count: %d\n", BTree_count(tree));
+    printf("Full Tree: \n");
+    
+    BTree_display(tree, printf_data, 4, '-');
+    
+    BTree_clear(tree);
+    
+    printf("After Clear: \n");
+    printf("Height: %d\n", BTree_height(tree));
+    printf("Degree: %d\n", BTree_degree(tree));
+    printf("Count: %d\n", BTree_count(tree));
+    
+    BTree_display(tree, printf_data, 4, '-');
+    
+    BTree_destroy(tree);
+    
+	return 0;
 }
